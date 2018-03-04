7ff56c7bc5da8078c07d3a60880708d8630ddad9
Weijia Wang <381152119@qq.com>
2018-03-04 22:38:28 +0800

    virtual render

--- dev/src/index.vue
+++ dev/src/index.vue
@@ -1,24 +1,56 @@
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.card {
+  background: #aaa;
+}
+</style>
+
 <template>
   <div id="app">
-    <VirtualCollection/>
+    <input v-model="amount"/>
+    <VirtualCollection :cellSizeAndPositionGetter="cellSizeAndPositionGetter" :collection="items" :height="500" :width="330">
+      <div slot="cell" slot-scope="props" class="card">{{props.data}}</div>
+    </VirtualCollection>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  components: {
+  name: "App",
+  data() {
+    return {
+      amount: 200
+    };
+  },
+  computed: {
+    items() {
+      const columnHeight = [0, 0, 0]
+      return new Array(+this.amount).fill(1).map((_, index) => {
+        const column = index % 3;
+        const height = 100 + 50 * Math.random()
+        const result = {
+          data: `item${index}`,
+          height,
+          width: 100,
+          x: column * 110,
+          y: columnHeight[column]
+        }
+        columnHeight[column] += height + 10
+        return result
+      })
+    }
+  },
+  methods: {
+    cellSizeAndPositionGetter(item, index) {
+      const { data, ...sizeAndPosition } = item
+      return sizeAndPosition
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/VirtualCollection.vue
+++ src/VirtualCollection.vue
@@ -1,18 +1,101 @@
 <style lang="less" scoped>
-    div {
-        font-size: 42px;
+    .vue-virtual-collection {
+        overflow: auto;
+        &-container {
+            position: relative;
+        }
+        .cell-container {
+            position: absolute;
+            top: 0;
+            background: #ddd;
+        }
     }
 </style>
 
 <template>
-  <div>{{text}}</div>
+    <div class="vue-virtual-collection" :style="outerStyle" @scroll="onScroll" ref="outer">
+        <div class="vue-virtual-collection-container" :style="scrollHeight">
+            <div v-for="(item, index) in displayItems" class="cell-container" :key="index" :style="getComputedStyle(item, index)">
+                <slot name="cell" :data="collection[item.index].data"></slot>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+    import Vue from 'vue'
     export default {
+        props: ['cellSizeAndPositionGetter', 'collection', 'height', 'width'],
         data() {
             return {
-                text: 'VirtualCollection'
+                displayItems: []
+            }
+        },
+        created() {
+            this.flushDisplayItems()
+        },
+        methods: {
+            getComputedStyle(item, index) {
+                const { width, height, x, y } = item
+                return {
+                    left: `${x}px`,
+                    top: `${y}px`,
+                    width: `${width}px`,
+                    height: `${height}px`
+                }
+            },
+            checkIfNeedRender(item, index) {
+                console.log(checkIfNeedRender)
+                return true
+            },
+            onScroll() {
+                const { scrollTop, scrollLeft } = this.$refs.outer
+                this.flushDisplayItems()
+            },
+            isInViewPort({ x, y, width, height }, { scrollTop, scrollLeft }) {
+                const { height: outerHeight, width: outerWidth } = this
+                if (x + width < scrollLeft || x > scrollLeft + outerWidth || y > outerHeight + scrollTop || y + height < scrollTop) {
+                    return false
+                } else {
+                    return true
+                }
+            },
+            flushDisplayItems() {
+                let scrollTop = 0
+                let scrollLeft = 0
+                if (this.$refs.outer) {
+                    scrollTop = this.$refs.outer.scrollTop
+                    scrollLeft = this.$refs.outer.scrollLeft
+                }
+                this.displayItems = this.cellSizeAndPosition.filter(sizeAndPosition => this.isInViewPort(sizeAndPosition, { scrollTop, scrollLeft }))
+            }
+        },
+        computed: {
+            cellSizeAndPosition() {
+                return this.collection.map((item, index) => ({
+                    ...this.cellSizeAndPositionGetter(item),
+                    index
+                }))
+            },
+            scrollHeight() {
+                let containerHeight = 0
+                const { cellSizeAndPosition } = this
+                cellSizeAndPosition.forEach(sizeAndPosition => {
+                    const { y, height } = sizeAndPosition
+                    const bottom = y + height
+                    if (bottom > height) {
+                        containerHeight = bottom
+                    }
+                })
+                return {
+                    height: containerHeight + 'px'
+                }
+            },
+            outerStyle() {
+                return {
+                    height: this.height + 'px',
+                    width: this.width + 'px'
+                }
             }
         }
     }
